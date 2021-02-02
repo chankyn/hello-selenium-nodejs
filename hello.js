f6be19f6fc27c7652c8a8a8547f8a37463af54f5
Miguel Hinojosa <mahinojosa@viewnext.com>
2021-02-02 15:16:23 +0100

    Ejemplo con google funcionando

--- hello.js
+++ hello.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
-const webdriver = require('selenium-webdriver');
-
+const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function hello() {
-    let driver = new webdriver.Builder().forBrowser('firefox').build();
+    let driver = new Builder()
+                        .forBrowser('firefox')
+                        .build();
 
     await driver.get('https://www.google.com/');
+
+    let frame = await driver.wait(until.elementLocated(By.xpath('//iframe')));
+    await driver.switchTo().frame(frame);
+
+    let button = await driver.wait(until.elementLocated(By.id('introAgreeButton')),5000);
+    await button.click();
+
+    await driver.switchTo().defaultContent();
+    await driver.findElement(By.name('q')).sendKeys('devops', Key.RETURN);
 })();
+
